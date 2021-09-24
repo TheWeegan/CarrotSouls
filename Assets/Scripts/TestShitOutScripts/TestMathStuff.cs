fb3aa0986c26ab489b787a0a95d112dd85d493d1
TheWeegan <markus.wik1997@gmail.com>
2021-09-24 15:13:26 +0200

    Now the code calculates the area of any mesh

--- Assets/Scripts/TestShitOutScripts/TestMathStuff.cs
+++ Assets/Scripts/TestShitOutScripts/TestMathStuff.cs
@@ -7,56 +7,34 @@ public class TestMathStuff : MonoBehaviour {
     public Mesh _mesh;
     public Canvas textCanvas;
 
-    Vector3 _cross = new Vector3();
-
     private void OnDrawGizmos() {
         Gizmos.color = Color.cyan;
-        //Gizmos.DrawMesh(_mesh);
-
-
-        Gizmos.color = Color.green;
-        for (int i = 0; i < 1; i++) {
-            Vector3 firstVertice = _mesh.vertices[_mesh.triangles[0]];
-            Vector3 secondVertice = _mesh.vertices[_mesh.triangles[1]];
-            Vector3 thirdVertice = _mesh.vertices[_mesh.triangles[2]];
-
-            //Gizmos.DrawLine(firstVertice, secondVertice);
-            Gizmos.DrawLine(secondVertice, thirdVertice);
-            Gizmos.DrawLine(thirdVertice, firstVertice);
-
-
-            //Gizmos.DrawLine(cross,cross.normalized);
-            //float verticecArea = cross.magnitude * 0.5f;
-            //meshesArea += verticecArea;
-
-        }
+        Gizmos.DrawMesh(_mesh);
     }
     // Start is called before the first frame update
     void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+        CalculateMeshArea();
+
+    }
+
+    void CalculateMeshArea() {
         float meshesArea = 0;
         for (int i = 0; i < _mesh.triangles.Length; i += 3) {
             Vector3 firstVertice = _mesh.vertices[_mesh.triangles[i]];
             Vector3 secondVertice = _mesh.vertices[_mesh.triangles[i + 1]];
             Vector3 thirdVertice = _mesh.vertices[_mesh.triangles[i + 2]];
 
-
-
-
-            //Vector3 cross = Vector3.Cross(distance1, distance2);
-            //float verticecArea = cross.magnitude * 0.5f;
-            //meshesArea += verticecArea;
-
+            Vector3 distance = thirdVertice - secondVertice;
+            Vector3 distance2 = firstVertice - thirdVertice;
+            Vector3 _cross = Vector3.Cross(distance, distance2);
+            meshesArea += _cross.magnitude * 0.5f;
         }
-        textCanvas.GetComponentInChildren<Text>().text = meshesArea.ToString();
-
+        textCanvas.GetComponentInChildren<Text>().text = meshesArea.ToString() + "m^2";
     }
-
-    // Update is called once per frame
-    void Update() {
-
-
-    }
-
 
 
 }
